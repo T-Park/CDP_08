383a6b585e2c5f55dc7fc9b6e4fef9f9820c9478
kth1234 <sb3816@naver.com>
2016-04-24 20:52:13 +0900

    percept barcode via image proto

--- raspberryPi/barcode_reader.py
+++ raspberryPi/barcode_reader.py
@@ -1,2 +1,36 @@
 # barcode_reader.py
 # take picture and get barcode
+# it use zxing
+#        python-zxing opensource
+
+import zxing
+import picamera
+
+class BarcodeReader():
+
+    camera = None
+    reader = None
+
+    image_file = "image.jpg"
+
+    def __init__(self):
+        self.camera = picamera.PiCamera()
+        self.reader = zxing.BarCodeReader("/home/pi/zxing/zxing")
+        
+    def take_picture(self):
+        self.camera.capture(self.image_file) # take picture
+                                # and store
+
+    def parse_barcode(self):
+        barcode = self.reader.decode(self.image_file) # get barcode using zxing
+                                            # library
+
+        if barcode == None :
+            return False, None
+        else :
+            return True, barcode.data
+        
+        
+        
+    
+    
--- raspberryPi/main.py
+++ raspberryPi/main.py
@@ -1,23 +1,36 @@
 # main.py
 # coincollecter's main function
 import coin_collecter as cc
+import barcode_reader as br
 
 global coin_collecter # coin_collecter
 global connection # connection to server
+global barcode_reader # barcode reader
 
-def init():
-    coin_collecter = cc.CoinCollecter()
-    # connection
+
+coin_collecter = cc.CoinCollecter()
+barcode_reader = br.BarcodeReader()    
+# connection need to add
+
+
+print(coin_collecter)
+
+      
 
 while True:
     print("메뉴를 선택하세요:")
     select = input()
 
-    if select == 1:
+    if select == "1":
         flag = False;
         print("카메라 앞에 바코드를 위치하여 주세요")
         for i in range(5):
+            barcode_reader.take_picture()
+            flag, number = barcode_reader.parse_barcode()
 
+            if flag == True:
+                break
+            
         if flag == False:
             print("인식에 실패했습니다")
         else :
