93194deeff4f36aeecb09d32c9ea4d1d336693b8
gusqls5826 <gusqls5826@naver.com>
2016-05-09 20:25:27 +0900

    0509

---
+++ POS/Gui/0509/Product_test.java
@@ -0,0 +1,47 @@
+package GUI;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+
+public class Product_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a;
+		Scanner scan = new Scanner(System.in);
+		Function f = new Function();
+		f.deserial();
+
+		do {
+			f.display();
+			a = scan.nextInt();
+
+			switch (a) {
+			case 1:
+				f.regist();
+				break;
+			case 2:
+				f.delete();
+				break;
+			case 3:
+				f.catalog();
+				break;
+			case 4:
+				f.search();
+				break;
+			case 0:
+				break;
+			default:
+				System.out.println("잘못 입력하셨습니다.\n");
+				break;
+
+			}
+		} while (a != 0);
+
+		System.out.println("종료합니다.\n");
+
+	}
+
+}
