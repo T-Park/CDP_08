f94c0a67447277e1e650e8e8a56c8edddf68e1c5
gusqls5826 <gusqls5826@naver.com>
2016-05-13 23:25:22 +0900

    0513 GUI

---
+++ POS/Gui/0513/SalesInventory.java
@@ -0,0 +1,20 @@
+package GUI;
+
+public class SalesInventory {
+
+	private String id;
+	private int inventory;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getInventory() {
+		return inventory;
+	}
+	public void setInventory(int inventory) {
+		this.inventory = inventory;
+	}
+}
