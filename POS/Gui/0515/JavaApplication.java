384d40cc0319c6805eab659c6221478a499b8e59
gusqls5826 <gusqls5826@naver.com>
2016-05-15 23:01:43 +0900

    0515

---
+++ POS/Gui/0515/JavaApplication.java
@@ -0,0 +1,24 @@
+package posClient;
+
+import GUI.*;
+
+/**
+ *
+ * @author ÀüÇöºó
+ */
+public class JavaApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    	
+        demo a = new demo();
+        a.setVisible(true);
+        System.out.println("½ÇÇàµÆ¾î¿ë");
+    }
+    
+    
+    
+}
---
+++ POS/Gui/0515/PAmt.java
@@ -0,0 +1,26 @@
+package GUI;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+
+public class PAmt {
+
+	// (상품코드 입력 -> 상품명, 상품가격 출력 -> "여기까지" 신호 보내면
+	// [입력 코드값 숫자아님?] 물품 총 수량 및 총액 출력.
+	// 배열에서 넘어오는 형태일것임. 따라서 배열중에 가격라인만 계속 받아서 가산하면 계산할 가격 나옴.
+
+	public static void main(String args[]) {
+
+		Function f = new Function();
+		
+		f.deserial();
+		Scanner scan = new Scanner(System.in);
+		
+	
+		f.cash();
+		
+		
+	}
+}
