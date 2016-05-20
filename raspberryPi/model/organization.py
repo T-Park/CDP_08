c946c89577e8e548bc99806e194fedc4bb8c3aa2
KTH1234 <sb3816@naver.com>
2016-05-20 19:25:14 +0900

    .

---
+++ raspberryPi/model/organization.py
@@ -0,0 +1,11 @@
+# organization.py
+# it contain organization's info
+
+class Organization():
+    def __init__(self, did=None, name=None, point=None, tel=None, type=None):
+        self.did = did
+        self.name = name
+        self.point = point
+        self.tel = tel
+        self.type = type
+
