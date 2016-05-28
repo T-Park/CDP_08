e155b1ebc46fa082a53de2f9b14204d2c37f97da
Java2-2011034018 <allen4485@naver.com>
2016-05-28 20:32:48 +0900

    5.28
    5.28

---
+++ POS/Gui/src/main/JavaApplication2.java
@@ -0,0 +1,24 @@
+package main;
+
+import GUI.*;
+
+/**
+ *
+ * @author ÀüÇöºó
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    	
+        demo a = new demo();
+        a.setVisible(true);
+        System.out.println("½ÇÇàµÆ¾î¿ë");
+    }
+    
+    
+    
+}
